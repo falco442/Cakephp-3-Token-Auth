5185b00b238cce1e3b9ba99d6db4fdc20d21c8de
falco442 <marcofra83@gmail.com>
2016-10-03 12:29:35 +0200

    First commit

---
+++ src/Auth/TokenAuthenticate.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Auth;
+
+use Cake\Auth\FormAuthenticate;
+use Cake\Network\Request;
+use Cake\Network\Response;
+use Cake\ORM\TableRegistry;
+
+class TokenAuthenticate extends FormAuthenticate
+{
+    public function getUser(Request $request){
+    	if(!$request->query('token'))
+    		return false;
+    	$table = TableRegistry::get('Users');
+    	$user = $table->findByToken($request->query('token'))->first()->toArray();
+    	return $user;
+    }
+}
